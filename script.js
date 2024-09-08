5cf7a9e9bd9ce6dfca0cd73abeabd7f7113a2378
Elijah Sheridan <elijahtsheridan25@gmail.com>
2024-09-08 15:18:52 -0400

    added active card functions

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-const panels = document.querySelector('.panel')
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach((panel)=>{
+    panel.addEventListener('click', ()=>{
+        removeActiveClass()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClass(){
+panels.forEach(panel=>{
+    panel.classList.remove('active')
+})
+}
